8ddd793e89eeef37fff6c6ce120de502adcb374b
Thomas <drahcira.tw@gmail.com>
2015-09-02 13:04:29 -0700

    adding stuff

--- script/email.php
+++ script/email.php
@@ -1,8 +1,11 @@
 <?php 
 
 	$to = 'tomadamwood@gmail.com';
-	$from = $_POST('company-name');
+	$from = $_POST('company-name'). '<br/><br/>' .$POST_('email');
 	$subject = 'Portfolio Website Email';
+	$phone = $_POST('phone');
+	$message = $_POST('desc'). '<br/><br/>' . $from. '<br/><br/>' .$phone;
 
+	mail($to,$subject,$message);
 
  ?>
